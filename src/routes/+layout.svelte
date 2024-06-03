fb51bd0a778185ca7786316680048c19736bf1eb
rob leroy <robleroy@msn.com>
2024-06-03 10:33:27 -0500

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script>
   import "./app.css";
 </script>
+
 <svelte:head>
-  <title></title>
-  <link rel="%sveltekit.assets%/apple-touch-icon" href="/apple-touch-icon-180x180.png"> 
+  <title>Tablature</title>
 </svelte:head>
+
 <slot />
 
 <style>
